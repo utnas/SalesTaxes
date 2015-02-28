dd50c911d67c0d0115e0d4980ae4c618609860fc
Dooley <nsewolo@gmail.com>
2015-02-27 19:45:13 -0500

    Abstract class added

--- src/main/java/com/teksystem/salestaxes/model/NoneTaxableImportedItem.java
+++ src/main/java/com/teksystem/salestaxes/model/NoneTaxableImportedItem.java
@@ -3,6 +3,7 @@ package com.teksystem.salestaxes.model;
 import com.teksystem.salestaxes.visitor.TaxVisitor;
 
 public final class NoneTaxableImportedItem extends AbstractItem {
+
     public NoneTaxableImportedItem(final String name, final Double price) {
         super(name, price);
     }
