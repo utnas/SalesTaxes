4ff7664ac63f620e77705f32c7963e5c224cc6a7
Dooley <nsewolo@gmail.com>
2015-03-09 22:05:09 -0400

    Is better to use constants than value inside functions

--- src/main/java/com/teksystem/salestaxes/utils/CustomDecimalFormatter.java
+++ src/main/java/com/teksystem/salestaxes/utils/CustomDecimalFormatter.java
@@ -7,6 +7,10 @@ import java.util.Locale;
 
 public class CustomDecimalFormatter {
     public final static MathContext MATH_CONTEXT = new MathContext(3, RoundingMode.HALF_UP);
+    public final static int MINIMUM_INTEGER_DIGITS = 1;
+    public final static int MAXIMUM_INTEGER_DIGITS = 9;
+    public final static int MINIMUM_FRACTION_DIGITS = 2;
+    public final static int MAXIMUM_FRACTION_DIGITS = 2;
 
     public static String format(final Number calculatedRate) {
         return applyCustomDecimalFormat(calculatedRate);
@@ -14,10 +18,10 @@ public class CustomDecimalFormatter {
 
     private static String applyCustomDecimalFormat(final Number number) {
         final NumberFormat decimalFormat = NumberFormat.getInstance(Locale.US);
-        decimalFormat.setMinimumIntegerDigits(1);
-        decimalFormat.setMaximumIntegerDigits(9);
-        decimalFormat.setMinimumFractionDigits(2);
-        decimalFormat.setMaximumFractionDigits(2);
+        decimalFormat.setMinimumIntegerDigits(MINIMUM_INTEGER_DIGITS);
+        decimalFormat.setMaximumIntegerDigits(MAXIMUM_INTEGER_DIGITS);
+        decimalFormat.setMinimumFractionDigits(MINIMUM_FRACTION_DIGITS);
+        decimalFormat.setMaximumFractionDigits(MAXIMUM_FRACTION_DIGITS);
         return decimalFormat.format(number);
     }
 
