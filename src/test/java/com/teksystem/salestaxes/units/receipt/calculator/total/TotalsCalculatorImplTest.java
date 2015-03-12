57a601add1f0f697846b0dcb8bbd4f26a5f85fa8
Dooley <nsewolo@gmail.com>
2015-03-11 23:41:06 -0400

    Mocking taxApplier

--- src/test/java/com/teksystem/salestaxes/units/receipt/calculator/total/TotalsCalculatorImplTest.java
+++ src/test/java/com/teksystem/salestaxes/units/receipt/calculator/total/TotalsCalculatorImplTest.java
@@ -1,36 +1,35 @@
 package com.teksystem.salestaxes.units.receipt.calculator.total;
 
+import com.teksystem.salestaxes.model.items.Item;
 import com.teksystem.salestaxes.model.items.NonTaxableImportedItem;
 import com.teksystem.salestaxes.model.items.TaxableImportedItem;
-import com.teksystem.salestaxes.model.tax.TaxVisitorImpl;
 import com.teksystem.salestaxes.receipt.calculator.tax.TaxApplierImpl;
 import com.teksystem.salestaxes.receipt.calculator.total.TotalsCalculator;
 import com.teksystem.salestaxes.receipt.calculator.total.TotalsCalculatorImpl;
 import com.teksystem.salestaxes.utils.NegativeDecimalException;
+import com.teksystem.salestaxes.utils.Pair;
 import org.junit.Test;
 
 import java.math.BigDecimal;
 
 import static com.teksystem.salestaxes.receipt.calculator.tax.TaxApplierHelper.addItemsTo;
+import static com.teksystem.salestaxes.units.receipt.calculator.TestHelper.createTaxApplier;
 import static com.teksystem.salestaxes.utils.CustomDecimalFormatter.format;
 import static java.util.Collections.EMPTY_LIST;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 public class TotalsCalculatorImplTest {
 
     @Test
     public void itShouldCalculateTotalOfSalesTaxes() throws NegativeDecimalException {
-        final TaxApplierImpl taxApplier = new TaxApplierImpl(new TaxVisitorImpl(10.0, 5.0));
-
+        final TaxApplierImpl taxApplier = createTaxApplier(10.0, 5.0);
         addItemsTo(taxApplier,
                 new NonTaxableImportedItem("imported box chocolates", new BigDecimal(10.00)),
                 new TaxableImportedItem("imported bottle of perfume", new BigDecimal(47.50))
         );
+        final TotalsCalculator totalsCalculator = createTotalCalculator(taxApplier.getTaxedItems());
 
-        final TotalsCalculatorImpl totalsCalculator = new TotalsCalculatorImpl(taxApplier.getTaxedItems());
         assertThat(totalsCalculator.calculateTotalOfSalesTaxes().doubleValue(), is(7.63));
     }
 
@@ -41,17 +40,22 @@ public class TotalsCalculatorImplTest {
 
     @Test
     public void itShouldCalculateTotalOfItemsIncludingTaxes() throws NegativeDecimalException {
-        final TaxApplierImpl taxApplier = new TaxApplierImpl(new TaxVisitorImpl(10.0, 5.0));
+        final TaxApplierImpl taxApplier = createTaxApplier(10.0, 5.0);
         addItemsTo(taxApplier,
                 new NonTaxableImportedItem("imported box chocolates", new BigDecimal(10.00)),
                 new TaxableImportedItem("imported bottle of perfume", new BigDecimal(47.50))
         );
-        final TotalsCalculatorImpl totalsCalculator = new TotalsCalculatorImpl(taxApplier.getTaxedItems());
+        final TotalsCalculator totalsCalculator = createTotalCalculator(taxApplier.getTaxedItems());
+
         assertThat(format(totalsCalculator.calculateTotalOfTaxedItems()), is("65.13"));
     }
 
     @Test
     public void itShouldReturnZeroAsTotalOfNoItem() {
         assertThat(new TotalsCalculatorImpl(EMPTY_LIST).calculateTotalOfTaxedItems().doubleValue(), is(0.0));
+    }
+
+    private static TotalsCalculator createTotalCalculator(Iterable<Pair<Item, BigDecimal>> taxedItems) {
+        return new TotalsCalculatorImpl(taxedItems);
     }
 }
