468dca84eaf911554a667cad9b2e680dbae744c3
Dooley <nsewolo@gmail.com>
2015-03-11 23:47:02 -0400

    Mock tests readability improvements

--- src/test/java/com/teksystem/salestaxes/units/receipt/calculator/total/TotalsCalculatorImplTest.java
+++ src/test/java/com/teksystem/salestaxes/units/receipt/calculator/total/TotalsCalculatorImplTest.java
@@ -13,6 +13,7 @@ import org.junit.Test;
 import java.math.BigDecimal;
 
 import static com.teksystem.salestaxes.receipt.calculator.tax.TaxApplierHelper.addItemsTo;
+import static com.teksystem.salestaxes.units.model.items.ItemMockHelper.mockItem;
 import static com.teksystem.salestaxes.units.receipt.calculator.TestHelper.createTaxApplier;
 import static com.teksystem.salestaxes.utils.CustomDecimalFormatter.format;
 import static java.util.Collections.EMPTY_LIST;
@@ -25,8 +26,8 @@ public class TotalsCalculatorImplTest {
     public void itShouldCalculateTotalOfSalesTaxes() throws NegativeDecimalException {
         final TaxApplierImpl taxApplier = createTaxApplier(10.0, 5.0);
         addItemsTo(taxApplier,
-                new NonTaxableImportedItem("imported box chocolates", new BigDecimal(10.00)),
-                new TaxableImportedItem("imported bottle of perfume", new BigDecimal(47.50))
+                mockItem("imported box chocolates", 10.00, NonTaxableImportedItem.class),
+                mockItem("imported bottle of perfume", 47.50, TaxableImportedItem.class)
         );
         final TotalsCalculator totalsCalculator = createTotalCalculator(taxApplier.getTaxedItems());
 
@@ -37,8 +38,8 @@ public class TotalsCalculatorImplTest {
     public void itShouldCalculateTotalOfItemsIncludingTaxes() throws NegativeDecimalException {
         final TaxApplierImpl taxApplier = createTaxApplier(10.0, 5.0);
         addItemsTo(taxApplier,
-                new NonTaxableImportedItem("imported box chocolates", new BigDecimal(10.00)),
-                new TaxableImportedItem("imported bottle of perfume", new BigDecimal(47.50))
+                mockItem("imported box chocolates", 10.00, NonTaxableImportedItem.class),
+                mockItem("imported bottle of perfume", 47.50, TaxableImportedItem.class)
         );
         final TotalsCalculator totalsCalculator = createTotalCalculator(taxApplier.getTaxedItems());
 
